b19b587879be293bf7c1d127c12bd5bdbe03c9d7
程巍巍 <littocats@gmail.com>
2017-05-24 21:37:01 +0800

    fix merge error

--- index.js
+++ index.js
@@ -11,11 +11,8 @@ path.relative(from, to)
 path.resolve([...paths])
 path.sep
 */
-<<<<<<< HEAD
+
 function _normalize(path) {
-=======
-function normalize(path) {
->>>>>>> fcebacbe70ed4ef6a0a3b3be464eec0f06e6e9b7
   valid(path);
   return `${path}/`.split(/\/+/g).reduce((previous, current) => {
     if (!Array.isArray(previous)) previous = [previous];
@@ -26,11 +23,7 @@ function normalize(path) {
   }).join('/');
 }
 
-<<<<<<< HEAD
 function _basename(path, ext) {
-=======
-function basename(path, ext) {
->>>>>>> fcebacbe70ed4ef6a0a3b3be464eec0f06e6e9b7
   var basename = path.split(/\//g).pop();
   if (ext) {
     var tmp = basename.split(/\./g);
@@ -42,29 +35,17 @@ function basename(path, ext) {
   return basename;
 }
 
-<<<<<<< HEAD
 function _dirname(path) {
   return path.split(/\//g).slice(0, -1).join('/')
 }
 
 function _extname(path) {
-=======
-function dirname(path) {
-  return path.split(/\//g).pop().join('/')
-}
-
-function extname(path) {
->>>>>>> fcebacbe70ed4ef6a0a3b3be464eec0f06e6e9b7
   var tmp = path.replace(/^[\.]+/, '');
   if (/\./.test(tmp)) return tmp.match(/\.[^.]*$/)[0];
   return '';
 }
 
-<<<<<<< HEAD
 function _format(options) {
-=======
-function format(options) {
->>>>>>> fcebacbe70ed4ef6a0a3b3be464eec0f06e6e9b7
   var {
     dir,
     root,
@@ -77,19 +58,11 @@ function format(options) {
   return normalize(`${_dir}/${_base}`);
 }
 
-<<<<<<< HEAD
 function _isAbsolute(path) {
   return /^\//.test(path)
 }
 
 function _parse(path) {
-=======
-function isAbsolute(path) {
-  return /^\//.test(path)
-}
-
-function parse(path) {
->>>>>>> fcebacbe70ed4ef6a0a3b3be464eec0f06e6e9b7
   var obj = {},
     tmp;
   var components = path.split(/\//g)
@@ -112,11 +85,7 @@ function parse(path) {
   return obj;
 }
 
-<<<<<<< HEAD
 function _resolve(segments) {
-=======
-function resolve(segments) {
->>>>>>> fcebacbe70ed4ef6a0a3b3be464eec0f06e6e9b7
   var flat = segments.reduce((previous, current) => {
     if (!Array.isArray(previous)) previous = [previous];
     if (Array.isArray(current)) Array.prototype.push.apply(previous, current);
@@ -136,7 +105,6 @@ function valid(path) {
 }
 
 export function normalize(path): string {
-<<<<<<< HEAD
   return _normalize(path);
 }
 export function basename(path, ext): string {
@@ -162,30 +130,3 @@ export function resolve(): string {
 }
 
 export default exports;
-=======
-  return normalize(path);
-}
-export function basename(path, ext): string {
-  return basename(normalize(path), ext);
-}
-export function dirname(path): string {
-  return dirname(normalize(path));
-}
-export function extname(path): string {
-  return extname(normalize(path))
-}
-export function format(options): string {
-  return format(options);
-}
-export function isAbsolute(path): boolean {
-  return isAbsolute(valid(path))
-}
-export function parse(path): string {
-  return parse(normalize(path))
-}
-export function resolve(): string {
-  return resolve.call({}, Array.prototype.slice.call(arguments, 0));
-}
-
-export default exports;
->>>>>>> fcebacbe70ed4ef6a0a3b3be464eec0f06e6e9b7
